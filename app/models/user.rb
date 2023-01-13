697942fa604e8d9ab0e7d7aa7c5c1f16b20037b1
crhistoff <cechevarriamesias@gmail.com>
2023-01-13 00:45:09 -0500

    add:active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+    enum role: { admin: 'landlord', user: 'homeseeker' }
+    has_many :property_users
+    has_many :properties, through: :property_users
+    validates :name, presence: true
+    validates :username, presence: true
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+    validates :password, length: { minimum: 6 }, allow_nil: true
 end
+  
